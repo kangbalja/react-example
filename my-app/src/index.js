ba7256498e8d358707b6d8477df7da70d7ed68e6
kangbalja <kangbalja@gmail.com>
2020-01-08 22:34:45 +0900

    element test2

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -6,8 +6,8 @@ import * as serviceWorker from './serviceWorker';
 
 const element = <h1>Hello, world</h1>;
 
-// ReactDOM.render(<App />, document.getElementById('root'));
-ReactDOM.render(element, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(element, document.getElementById('root2'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
